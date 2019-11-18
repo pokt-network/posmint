70114e99cf96d7b4dde115dc71632ff7a2106d67
Luis de Leon <luyzdeleon@gmail.com>
2019-11-18 14:41:54 -0400

    Updated LazyKeybase implementation with latest Keybase interface / tests WIP

--- crypto/keys/lazy_keybase.go
+++ crypto/keys/lazy_keybase.go
@@ -6,7 +6,7 @@ import (
 	"github.com/tendermint/tendermint/crypto"
 	cmn "github.com/tendermint/tendermint/libs/common"
 
-	"github.com/pokt-network/posmint/crypto/keys/hd"
+	"github.com/pokt-network/posmint/types"
 	sdk "github.com/pokt-network/posmint/types"
 )
 
@@ -26,7 +26,7 @@ func New(name, dir string) Keybase {
 	return lazyKeybase{name: name, dir: dir}
 }
 
-func (lkb lazyKeybase) List() ([]Info, error) {
+func (lkb lazyKeybase) List() ([]KeyPair, error) {
 	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
@@ -36,186 +36,94 @@ func (lkb lazyKeybase) List() ([]Info, error) {
 	return newDbKeybase(db).List()
 }
 
-func (lkb lazyKeybase) Get(name string) (Info, error) {
+func (lkb lazyKeybase) Get(address types.AccAddress) (KeyPair, error) {
 	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
 	}
 	defer db.Close()
 
-	return newDbKeybase(db).Get(name)
+	return newDbKeybase(db).Get(address)
 }
 
-func (lkb lazyKeybase) GetByAddress(address sdk.AccAddress) (Info, error) {
-	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return nil, err
-	}
-	defer db.Close()
-
-	return newDbKeybase(db).GetByAddress(address)
-}
-
-func (lkb lazyKeybase) Delete(name, passphrase string, skipPass bool) error {
+func (lkb lazyKeybase) Delete(address types.AccAddress, passphrase string, skipPass bool) error {
 	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
-	return newDbKeybase(db).Delete(name, passphrase, skipPass)
+	return newDbKeybase(db).Delete(address, passphrase, skipPass)
 }
 
-func (lkb lazyKeybase) Sign(name, passphrase string, msg []byte) ([]byte, crypto.PubKey, error) {
+func (lkb lazyKeybase) Update(address types.AccAddress, oldpass string, newpass string) error {
+	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return newDbKeybase(db).Update(address, oldpass, newpass)
+}
+
+func (lkb lazyKeybase) Sign(address types.AccAddress, passphrase string, msg []byte) ([]byte, crypto.PubKey, error) {
 	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, nil, err
 	}
 	defer db.Close()
 
-	return newDbKeybase(db).Sign(name, passphrase, msg)
+	return newDbKeybase(db).Sign(address, passphrase, msg)
 }
 
-func (lkb lazyKeybase) CreateMnemonic(name string, language Language, passwd string, algo SigningAlgo) (info Info, seed string, err error) {
+func (lkb lazyKeybase) CreateMnemonic(bip39Passwd string, passwd string) (kp KeyPair, mnemonic string, err error) {
 	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, "", err
 	}
 	defer db.Close()
 
-	return newDbKeybase(db).CreateMnemonic(name, language, passwd, algo)
+	return newDbKeybase(db).CreateMnemonic(bip39Passwd, passwd)
 }
 
-func (lkb lazyKeybase) CreateAccount(name, mnemonic, bip39Passwd, encryptPasswd string, account uint32, index uint32) (Info, error) {
+func (lkb lazyKeybase) DeriveFromMnemonic(mnemonic, bip39Passwd, encryptPasswd string) (KeyPair, error) {
 	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return nil, err
 	}
 	defer db.Close()
 
-	return newDbKeybase(db).CreateAccount(name, mnemonic, bip39Passwd, encryptPasswd, account, index)
+	return newDbKeybase(db).Derive(mnemonic, bip39Passwd, encryptPasswd)
 }
 
-func (lkb lazyKeybase) Derive(name, mnemonic, bip39Passwd, encryptPasswd string, params hd.BIP44Params) (Info, error) {
-	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return nil, err
-	}
-	defer db.Close()
-
-	return newDbKeybase(db).Derive(name, mnemonic, bip39Passwd, encryptPasswd, params)
-}
-
-func (lkb lazyKeybase) CreateLedger(name string, algo SigningAlgo, hrp string, account, index uint32) (info Info, err error) {
-	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return nil, err
-	}
-	defer db.Close()
-
-	return newDbKeybase(db).CreateLedger(name, algo, hrp, account, index)
-}
-
-func (lkb lazyKeybase) CreateOffline(name string, pubkey crypto.PubKey) (info Info, err error) {
-	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return nil, err
-	}
-	defer db.Close()
-
-	return newDbKeybase(db).CreateOffline(name, pubkey)
-}
-
-func (lkb lazyKeybase) CreateMulti(name string, pubkey crypto.PubKey) (info Info, err error) {
-	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return nil, err
-	}
-	defer db.Close()
-
-	return newDbKeybase(db).CreateMulti(name, pubkey)
-}
-
-func (lkb lazyKeybase) Update(name, oldpass string, getNewpass func() (string, error)) error {
+func (lkb lazyKeybase) ImportPrivKey(armor, decryptPassphrase, encryptPassphrase string) error {
 	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
-	return newDbKeybase(db).Update(name, oldpass, getNewpass)
+	return newDbKeybase(db).ImportPrivKey(armor, decryptPassphrase, encryptPassphrase)
 }
 
-func (lkb lazyKeybase) Import(name string, armor string) (err error) {
+func (lkb lazyKeybase) ExportPrivKeyEncryptedArmor(address types.AccAddress, decryptPassphrase, encryptPassphrase string) (armor string, err error) {
 	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
-	return newDbKeybase(db).Import(name, armor)
+	return newDbKeybase(db).ExportPrivKeyEncryptedArmor(address, decryptPassphrase, encryptPassphrase)
 }
 
-func (lkb lazyKeybase) ImportPrivKey(name string, armor string, passphrase string) error {
+func (lkb lazyKeybase) ExportPrivateKeyObject(address types.AccAddress, passphrase string) (crypto.PrivKey, error) {
 	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
-	return newDbKeybase(db).ImportPrivKey(name, armor, passphrase)
-}
-
-func (lkb lazyKeybase) ImportPubKey(name string, armor string) (err error) {
-	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	return newDbKeybase(db).ImportPubKey(name, armor)
-}
-
-func (lkb lazyKeybase) Export(name string) (armor string, err error) {
-	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return "", err
-	}
-	defer db.Close()
-
-	return newDbKeybase(db).Export(name)
-}
-
-func (lkb lazyKeybase) ExportPubKey(name string) (armor string, err error) {
-	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return "", err
-	}
-	defer db.Close()
-
-	return newDbKeybase(db).ExportPubKey(name)
-}
-
-func (lkb lazyKeybase) ExportPrivateKeyObject(name string, passphrase string) (crypto.PrivKey, error) {
-	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return nil, err
-	}
-	defer db.Close()
-
-	return newDbKeybase(db).ExportPrivateKeyObject(name, passphrase)
-}
-
-func (lkb lazyKeybase) ExportPrivKey(name string, decryptPassphrase string,
-	encryptPassphrase string) (armor string, err error) {
-
-	db, err := sdk.NewLevelDB(lkb.name, lkb.dir)
-	if err != nil {
-		return "", err
-	}
-	defer db.Close()
-
-	return newDbKeybase(db).ExportPrivKey(name, decryptPassphrase, encryptPassphrase)
+	return newDbKeybase(db).ExportPrivateKeyObject(address, passphrase)
 }
 
 func (lkb lazyKeybase) CloseDB() {}
