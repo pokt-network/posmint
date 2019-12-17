3cd7f55ea6d0ab45362d13b470f700310bee0e27
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2019-12-17 12:22:09 -0400

    set default default proposer reward percentage; use common_test utility

--- x/pos/keeper/reward_test.go
+++ x/pos/keeper/reward_test.go
@@ -1,56 +1,53 @@
 package keeper
 
 import (
-	"github.com/pokt-network/posmint/testsUtil"
 	"encoding/hex"
-	"github.com/pokt-network/posmint/baseapp"
-	"github.com/pokt-network/posmint/codec"
 	sdk "github.com/pokt-network/posmint/types"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 
-var capKey = sdk.NewKVStoreKey(baseapp.MainStoreKey)
-
 type args struct {
-	ctx     sdk.Context
-	amount  sdk.Int
-	address sdk.ValAddress
+	amount      sdk.Int
+	valAddress  sdk.ValAddress
+	consAddress sdk.ConsAddress
 }
 
-func TestSetValidatorAward(t *testing.T) {
+func TestSetandGetValidatorAward(t *testing.T) {
+	initialPower := int64(100)
+	nAccs := int64(4)
 	addressBytes := []byte("abcdefghijklmnopqrst")
 	validatorAddress, err := sdk.ValAddressFromHex(hex.EncodeToString(addressBytes))
 	if err != nil {
 		panic(err)
 	}
-	minGasPrices := sdk.DecCoins{sdk.NewInt64DecCoin("stake", 5000)}
-	options := baseapp.SetMinGasPrices(minGasPrices.String())
-	cdc := testUtil.MakeCodec()
-	bapp := testUtil.GetNewApp(capKey, cdc, options)
 
 	tests := []struct {
 		name          string
 		args          args
 		expectedCoins sdk.Int
-		expectedFind bool
-		app           *baseapp.BaseApp
-		keeper        Keeper
+		expectedFind  bool
 	}{
 		{
-			name:          "can set Value",
+			name:          "can set award",
 			expectedCoins: sdk.NewInt(1),
-			expectedFind:	true,
-			app:           bapp,
-			args:          args{ctx: testUtil.GetNewContext(bapp), amount: sdk.NewInt(int64(1)), address: validatorAddress},
-			keeper:        getNewKeeper(capKey, cdc),
+			expectedFind:  true,
+			args:          args{amount: sdk.NewInt(int64(1)), valAddress: validatorAddress},
+		},
+		{
+			name:          "can get award",
+			expectedCoins: sdk.NewInt(2),
+			expectedFind:  true,
+			args:          args{amount: sdk.NewInt(int64(2)), valAddress: validatorAddress},
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			test.keeper.setValidatorAward(test.args.ctx, test.args.amount, test.args.address)
-			coins, found := test.keeper.getValidatorAward(test.args.ctx, test.args.address)
+			context, _, keeper := createTestInput(t, true, initialPower, nAccs)
+
+			keeper.setValidatorAward(context, test.args.amount, test.args.valAddress)
+			coins, found := keeper.getValidatorAward(context, test.args.valAddress)
 			assert.Equal(t, test.expectedCoins, coins, "coins don't match")
 			assert.Equal(t, test.expectedFind, found, "finds don't match")
 
@@ -58,6 +55,91 @@ func TestSetValidatorAward(t *testing.T) {
 	}
 }
 
-func getNewKeeper(key sdk.StoreKey, cdc *codec.Codec) Keeper {
-	return Keeper{storeKey: key, cdc: cdc}
+func TestSetAndGetProposer(t *testing.T) {
+	initialPower := int64(100)
+	nAccs := int64(4)
+	addressBytes := []byte("abcdefghijklmnopqrst")
+	consAddress, err := sdk.ConsAddressFromHex(hex.EncodeToString(addressBytes))
+	if err != nil {
+		panic(err)
+	}
+
+	tests := []struct {
+		name            string
+		args            args
+		expectedAddress sdk.ConsAddress
+	}{
+		{
+			name:            "can set the preivous proposer",
+			args:            args{consAddress: consAddress},
+			expectedAddress: consAddress,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			context, _, keeper := createTestInput(t, true, initialPower, nAccs)
+
+			keeper.SetPreviousProposer(context, test.args.consAddress)
+			receivedAddress := keeper.GetPreviousProposer(context)
+			assert.Equal(t, test.expectedAddress, receivedAddress, "addresses do not match ")
+		})
+	}
+}
+
+func TestDeleteValidatorAward(t *testing.T) {
+	initialPower := int64(100)
+	nAccs := int64(4)
+	addressBytes := []byte("abcdefghijklmnopqrst")
+	validatorAddress, err := sdk.ValAddressFromHex(hex.EncodeToString(addressBytes))
+	if err != nil {
+		panic(err)
+	}
+
+	tests := []struct {
+		name          string
+		args          args
+		expectedCoins sdk.Int
+		expectedFind  bool
+	}{
+		{
+			name:          "can delete award",
+			expectedCoins: sdk.NewInt(0),
+			expectedFind:  false,
+			args:          args{amount: sdk.NewInt(int64(1)), valAddress: validatorAddress},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			context, _, keeper := createTestInput(t, true, initialPower, nAccs)
+
+			keeper.setValidatorAward(context, test.args.amount, test.args.valAddress)
+			keeper.deleteValidatorAward(context, test.args.valAddress)
+			_, found := keeper.getValidatorAward(context, test.args.valAddress)
+			assert.Equal(t, test.expectedFind, found, "finds do not match")
+
+		})
+	}
+}
+
+func TestGetProposerRewardPercentage(t *testing.T) {
+	initialPower := int64(100)
+	nAccs := int64(4)
+	tests := []struct {
+		name               string
+		expectedPercentage sdk.Int
+	}{
+		{
+			name:               "get reward percentage",
+			expectedPercentage: sdk.NewInt(90),
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			context, _, keeper := createTestInput(t, true, initialPower, nAccs)
+
+			percentage := keeper.getProposerRewardPercentage(context) // TODO: replace with  sdk.Dec isntead of sdk.Int
+			assert.Equal(t, test.expectedPercentage, percentage, "percentages do not match")
+		})
+	}
 }
