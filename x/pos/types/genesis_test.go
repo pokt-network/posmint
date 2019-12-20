83e0a735317d39625ec7636c25f7a36b1e8d03af
Otto V <ottoevargas@gmail.com>
2019-12-20 10:11:34 -0400

    more testing done

--- x/pos/types/genesis_test.go
+++ x/pos/types/genesis_test.go
@@ -1,27 +1,25 @@
 package types
 
 import (
+	"github.com/pokt-network/posmint/types"
 	"reflect"
 	"testing"
-
-	sdk "github.com/pokt-network/posmint/types"
-	"github.com/pokt-network/posmint/x/auth/types"
 )
 
 func TestDefaultGenesisState(t *testing.T) {
 	tests := []struct {
 		name string
-		want types.GenesisState
-	}{{"defaultState", types.GenesisState{
+		want GenesisState
+	}{{"defaultState", GenesisState{
 		Params:       DefaultParams(),
 		SigningInfos: make(map[string]ValidatorSigningInfo),
 		MissedBlocks: make(map[string][]MissedBlock),
-		DAO:          DAOPool(NewPool(sdk.ZeroInt())),
+		DAO:          DAOPool(NewPool(types.ZeroInt())),
 	}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := types.DefaultGenesisState(); !reflect.DeepEqual(got, tt.want) {
+			if got := DefaultGenesisState(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("DefaultGenesisState() = %v, want %v", got, tt.want)
 			}
 		})
@@ -30,12 +28,12 @@ func TestDefaultGenesisState(t *testing.T) {
 
 func TestNewGenesisState(t *testing.T) {
 	type args struct {
-		params           types.Params
-		validators       []types.Validator
-		dao              types.DAOPool
-		previousProposer sdk.ConsAddress
-		signingInfos     map[string]types.ValidatorSigningInfo
-		missedBlocks     map[string][]types.MissedBlock
+		params           Params
+		validators       []Validator
+		dao              DAOPool
+		previousProposer types.ConsAddress
+		signingInfos     map[string]ValidatorSigningInfo
+		missedBlocks     map[string][]MissedBlock
 	}
 	tests := []struct {
 		name string
