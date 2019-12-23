ce15a90e8b4f142f6f1719b29e6cab821ee63502
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2019-12-23 12:26:18 -0400

    add dao tests

--- x/pos/keeper/reward_test.go
+++ x/pos/keeper/reward_test.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"encoding/hex"
+	"fmt"
 	sdk "github.com/pokt-network/posmint/types"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -155,18 +156,43 @@ func TestMint(t *testing.T) {
 	tests := []struct {
 		name   string
 		amount sdk.Int
+		expected string
+		address sdk.ValAddress
+		panics bool
 	}{
 		{
 			name:   "mints a coin",
 			amount: sdk.NewInt(90),
+			expected: fmt.Sprintf("was successfully minted to %s", validatorAddress.String()),
+			address: validatorAddress,
+			panics: false,
+		},
+		{
+			name:               "panics invalid ammount of coins",
+			amount: sdk.NewInt(-1),
+			expected: fmt.Sprintf("negative coin amount: -1"),
+			address: validatorAddress,
+			panics: true,
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			context, _, keeper := createTestInput(t, true, initialPower, nAccs)
 
-			result := keeper.mint(context, test.amount, validatorAddress)
-			assert.Contains(t, result.Log, "was successfully minted", "does not contain message")
+			switch test.panics{
+			case true:
+				defer func () {
+					err := recover().(error)
+					assert.Contains(t, err.Error(), test.expected, "error does not match")
+				}()
+				_ = keeper.mint(context, test.amount, test.address)
+			default:
+				result := keeper.mint(context, test.amount, test.address)
+				assert.Contains(t, result.Log, test.expected, "does not contain message")
+				coins := keeper.coinKeeper.GetCoins(context, sdk.AccAddress(test.address))
+				assert.Equal(t, sdk.NewCoins(sdk.NewCoin(keeper.StakeDenom(context), test.amount)), coins, "coins should match")
+			}
 		})
 	}
 }
+
