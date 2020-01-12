475099c06a812dfec80323037c0d3800ce0b6929
andrewnguyen22 <amnguyen1@mail.usf.edu>
2020-01-12 17:59:33 -0500

    fixed crypto ed25519.go file

--- crypto/ed25519.go
+++ crypto/ed25519.go
@@ -1,35 +1,40 @@
 package crypto
 
 import (
+	ed255192 "crypto/ed25519"
 	"encoding/hex"
 	sdk "github.com/pokt-network/posmint/types"
 	"github.com/tendermint/tendermint/crypto/ed25519"
-	cryptoAmino "github.com/tendermint/tendermint/crypto/encoding/amino"
 )
 
 type PublicKey ed25519.PubKeyEd25519
 type PrivateKey ed25519.PrivKeyEd25519
 
 const (
+	PrivKeySize   = ed255192.PrivateKeySize
 	PubKeySize    = ed25519.PubKeyEd25519Size
 	SignatureSize = ed25519.SignatureSize
 )
 
-func NewPublicKey(hexString string) (*PublicKey, error) {
+func NewPublicKey(hexString string) (PublicKey, error) {
 	b, err := hex.DecodeString(hexString)
 	if err != nil {
-		return nil, err
+		return PublicKey{}, err
 	}
-	pubkey, err := cryptoAmino.PubKeyFromBytes(b)
-	if err != nil {
-		return nil, err
-	}
-	pk := PublicKey(pubkey.(ed25519.PubKeyEd25519))
-	return &pk, nil
+	var bz [PubKeySize]byte
+	copy(bz[:], b)
+	pubkey := ed25519.PubKeyEd25519(bz)
+	pk := PublicKey(pubkey)
+	return pk, nil
+}
+
+func (pub PublicKey) AminoBytes() []byte {
+	return ed25519.PubKeyEd25519(pub).Bytes()
 }
 
 func (pub PublicKey) Bytes() []byte {
-	return ed25519.PubKeyEd25519(pub).Bytes()
+	pkBytes := [PubKeySize]byte(pub)
+	return pkBytes[:]
 }
 
 func (pub PublicKey) String() string {
@@ -45,6 +50,11 @@ func (pub PublicKey) VerifySignature(msg []byte, sig []byte) bool {
 }
 
 func (priv PrivateKey) Bytes() []byte {
+	pkBytes := [PrivKeySize]byte(priv)
+	return pkBytes[:]
+}
+
+func (priv PrivateKey) AminoBytes() []byte {
 	return ed25519.PrivKeyEd25519(priv).Bytes()
 }
 
