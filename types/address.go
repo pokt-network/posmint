826242524d2ff732f88bddf2637f3edf865258b9
Otto V <ottoevargas@gmail.com>
2020-01-31 22:41:57 -0400

    standardized UPPERCASE in addresses

--- types/address.go
+++ types/address.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pokt-network/posmint/crypto"
 	tmCrypto "github.com/tendermint/tendermint/crypto"
 	"gopkg.in/yaml.v2"
+	"strings"
 )
 
 const (
@@ -53,7 +54,7 @@ func AddressFromHex(address string) (addr Address, err error) {
 		return Address{}, nil
 	}
 
-	bz, err := hex.DecodeString(address)
+	bz, err := hex.DecodeString(strings.ToUpper(address))
 	if err != nil {
 		return nil, err
 	}
@@ -152,7 +153,7 @@ func (aa Address) String() string {
 		return ""
 	}
 
-	str := hex.EncodeToString(aa.Bytes())
+	str := strings.ToUpper(hex.EncodeToString(aa.Bytes()))
 
 	return str
 }
