4401108d9a97ae9f567900ec233fd1377f6108fb
Luis de Leon <luyzdeleon@gmail.com>
2020-04-07 22:41:33 -0400

    Added message event emission to MsgChangeParam, MsgDAOTransfer and MsgUpgrade handlers

--- x/gov/keeper/dao_test.go
+++ x/gov/keeper/dao_test.go
@@ -5,6 +5,7 @@ import (
 	sdk "github.com/pokt-network/posmint/types"
 	"github.com/pokt-network/posmint/x/gov/types"
 	"github.com/stretchr/testify/assert"
+	abci "github.com/tendermint/tendermint/abci/types"
 	"testing"
 )
 
@@ -33,10 +34,25 @@ func TestKeeper_GetDAOBurn(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, daoActualCoins.Int64(), int64(1000))
 	assert.Equal(t, k.GetDAOTokens(ctx).Int64(), daoActualCoins.Int64())
-	k.DAOBurn(ctx, k.GetDAOOwner(ctx), sdk.OneInt())
+	daoBurnResult := k.DAOBurn(ctx, k.GetDAOOwner(ctx), sdk.OneInt())
 	acc = k.GetDAOAccount(ctx)
 	daoActualCoins = acc.GetCoins().AmountOf(sdk.DefaultStakeDenom)
 	assert.Equal(t, daoActualCoins.Int64(), int64(999))
+	// Test "message.sender" event emission
+	assert.Equal(
+		t,
+		true,
+		ContainsEvent(
+			daoBurnResult.Events,
+			abci.Event(
+				sdk.NewEvent(
+					sdk.EventTypeMessage,
+					sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+					sdk.NewAttribute(sdk.AttributeKeySender, k.GetDAOOwner(ctx).String()),
+				),
+			),
+		),
+	)
 }
 
 func TestKeeper_GetDAOTransfer(t *testing.T) {
@@ -49,8 +65,22 @@ func TestKeeper_GetDAOTransfer(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, daoActualCoins.Int64(), int64(1000))
 	assert.Equal(t, k.GetDAOTokens(ctx).Int64(), daoActualCoins.Int64())
-	k.DAOTransferFrom(ctx, k.GetDAOOwner(ctx), k.AuthKeeper.GetModuleAddress("FAKE"), sdk.NewInt(1))
+	daoTransferResult := k.DAOTransferFrom(ctx, k.GetDAOOwner(ctx), k.AuthKeeper.GetModuleAddress("FAKE"), sdk.NewInt(1))
 	acc = k.AuthKeeper.GetModuleAccount(ctx, "FAKE")
 	assert.Equal(t, acc.GetCoins().AmountOf(sdk.DefaultStakeDenom).Int64(), sdk.OneInt().Int64())
 	assert.Equal(t, k.GetDAOTokens(ctx).Int64(), int64(999))
+	// Test "message.sender" event emission
+	assert.Equal(
+		t,
+		true,
+		ContainsEvent(
+			daoTransferResult.Events,
+			abci.Event(
+				sdk.NewEvent(
+					sdk.EventTypeMessage,
+					sdk.NewAttribute(sdk.AttributeKeySender, k.GetDAOAccount(ctx).GetAddress().String()),
+				),
+			),
+		),
+	)
 }
