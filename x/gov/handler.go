4945eda8e6e85ee2bea1fb7ca8b8c256469891c7
Otto V <ottoevargas@gmail.com>
2020-05-30 21:06:10 -0400

    hotfix

--- x/gov/handler.go
+++ x/gov/handler.go
@@ -1,7 +1,6 @@
 package gov
 
 import (
-	"encoding/json"
 	"fmt"
 	sdk "github.com/pokt-network/posmint/types"
 	"github.com/pokt-network/posmint/x/gov/keeper"
@@ -26,27 +25,7 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 }
 
 func handleMsgChangeParam(ctx sdk.Ctx, msg types.MsgChangeParam, k keeper.Keeper) sdk.Result {
-	var value interface{}
-
-	err := json.Unmarshal(msg.ParamVal, &value)
-	if err != nil {
-		ctx.Logger().Error(fmt.Sprintf("unable to convert paramVal %s", msg.ParamVal))
-		return sdk.Result{Events: ctx.EventManager().Events()}
-	}
-	//to solve issue json unmarshalling numbers to float
-	toFloat, ok := value.(float64)
-	if ok {
-		value = int64(toFloat)
-	}
-
-	// if modifying acl, convert into map ACL for efficiency
-	if msg.ParamKey == types.NewACLKey(ModuleName, string(types.ACLKey)) {
-		acl, ok := value.(types.ACL)
-		if ok {
-			value = acl
-		}
-	}
-	return k.ModifyParam(ctx, msg.ParamKey, value, msg.FromAddress)
+	return k.ModifyParam(ctx, msg.ParamKey, msg.ParamVal, msg.FromAddress)
 }
 
 func handleMsgDaoTransfer(ctx sdk.Ctx, msg types.MsgDAOTransfer, k keeper.Keeper) sdk.Result {
@@ -64,5 +43,5 @@ func handleMsgDaoTransfer(ctx sdk.Ctx, msg types.MsgDAOTransfer, k keeper.Keeper
 }
 
 func handleMsgUpgrade(ctx sdk.Ctx, msg types.MsgUpgrade, k keeper.Keeper) sdk.Result {
-	return k.ModifyParam(ctx, types.NewACLKey(ModuleName, string(types.UpgradeKey)), msg.Upgrade, msg.Address)
+	return k.HandleUpgrade(ctx, types.NewACLKey(ModuleName, string(types.UpgradeKey)), msg.Upgrade, msg.Address)
 }
